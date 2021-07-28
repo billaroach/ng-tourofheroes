b092aded5c01cc96b375e53a72e687460d7ba0ef
artem <jesuisartem@gmai.com>
2021-07-28 17:53:47 +0500

    Final

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {HeroService} from "../hero.service";
-import {Hero} from '../hero'
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,20 +9,29 @@ import {Hero} from '../hero'
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  constructor(private heroService: HeroService) { }
-
   heroes!: Hero[];
 
-  ngOnInit(): void {
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
     this.getHeroes();
   }
 
-  getHeroes():void {
+  getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => {
-        this.heroes = heroes;
-      })
+      .subscribe(heroes => this.heroes = heroes);
   }
 
-
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {return;}
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      })
+  }
+  delete(hero:Hero):void {
+    this.heroes = this.heroes.filter(h => h!==hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
